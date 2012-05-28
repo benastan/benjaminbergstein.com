a7e337755fe685088a670f20c24b246649571c43
Ben Bergstein <bennyjbergstein@gmail.com>
2012-05-28 04:43:37 +0000

    Removing unnecessary files

--- benbergstein/views.py
+++
@@ -1,8 +0,0 @@
-# Create your views here.
-from django.template import Template, Context
-from django.template.loader import get_template
-from django.http import HttpResponse
-from django.core.paginator import Paginator, EmptyPage
-from articles.models import Article
-from django.views.decorators.cache import cache_page
-
